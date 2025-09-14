373839b91a5a3062438ed291723488055059fb0c
YOUSOL <imjamminii@gmail.com>
2025-09-14 21:32:52 +0900

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ const ClubPage: React.FC = () => {
       <iframe
         width="560"
         height="315"
-        src="https://www.youtube.com/embed/3JXDh3knK34"
+        src="https://www.youtube.com/embed/25q75Ca2nOQ"
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
