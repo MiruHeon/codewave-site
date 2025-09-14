189e014d5d4ffd8f4197aaf2aadbccd88e8b1477
YOUSOL <imjamminii@gmail.com>
2025-09-14 12:07:30 +0900

    page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1>안녕하세요</h1>;
+  return (
+    <h1 style={{
+      color: "white",
+      background: "linear-gradient(to right, #ff7e5f, #feb47b)",
+      padding: "50px",
+      textAlign: "center",
+      borderRadius: "20px",
+      fontSize: "3rem"
+    }}>
+      안녕하세요
+    </h1>
+  );
 }
